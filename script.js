65e34158524aeaf34cbd673282812925dc57a424
Denrubka <74959243+Denrubka@users.noreply.github.com>
2021-05-30 15:35:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-works', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
